d1223f353a12b4901efba2c851e067368ed24250
Prabin Rath <prabinrath123@gmail.com>
2022-05-12 20:04:20 +0530

    Create py-connector.py

---
+++ SQL/py-connector.py
@@ -0,0 +1,16 @@
+import pyodbc
+server = 'naamkaran.database.windows.net'
+database = 'NaamKaran-DB'
+username = 'naamkaran'
+password = '{NM@wfh2022}'   
+driver= '{ODBC Driver 17 for SQL Server}'
+
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+    with conn.cursor() as cursor:
+        # cursor.execute("SELECT * FROM Users")
+        # row = cursor.fetchone()
+        # while row:
+        #     print (row)
+        #     row = cursor.fetchone()
+
+        cursor.execute("INSERT INTO Audio(AudioB64) VALUES (CONVERT(varbinary(max), 'World_Music'))")
