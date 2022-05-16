d97844e886ae751606015fce728032c9679cd8ab
Prabin Rath <prabinrath123@gmail.com>
2022-05-16 12:55:18 +0530

    Update demo-request.py

--- util/demo-request.py
+++ util/demo-request.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-# base_url = "https://naamkaran-db-gateway.azurewebsites.net"
-base_url = "http://127.0.0.1:5000"
+base_url = "https://naamkaran-db-gateway.azurewebsites.net"
+# base_url = "http://127.0.0.1:5000"
 
 # url = base_url + '/insert_user'
 # payload = {"UserId": "'u791554'",
@@ -20,15 +20,15 @@ base_url = "http://127.0.0.1:5000"
 # x = requests.post(url, json = payload)
 # print(x.text)
 
-# url = base_url + '/get_user'
-# payload = {"UserId": "'u791553'", "option": "select"}
-# x = requests.post(url, json = payload)
-# print(x.text)
+url = base_url + '/get_user'
+payload = {"UserId": "'u791553'", "option": "select"}
+x = requests.post(url, json = payload)
+print(x.text)
 
-# url = base_url + '/get_user'
-# payload = {"option": "all"}
-# x = requests.post(url, json = payload)
-# print(x.text)
+url = base_url + '/get_user'
+payload = {"option": "all"}
+x = requests.post(url, json = payload)
+print(x.text)
 
 # url = base_url + '/insert_name'
 # payload = {"PrefName": "'Asween'",
@@ -37,15 +37,15 @@ base_url = "http://127.0.0.1:5000"
 # x = requests.post(url, json = payload)
 # print(x.text)
 
-# url = base_url + '/get_name'
-# payload = {"NameId": 2001, "option": "select"}
-# x = requests.post(url, json = payload)
-# print(x.text)
+url = base_url + '/get_name'
+payload = {"NameId": 2001, "option": "select"}
+x = requests.post(url, json = payload)
+print(x.text)
 
-# url = base_url + '/get_name'
-# payload = {"option": "all"}
-# x = requests.post(url, json = payload)
-# print(x.text)
+url = base_url + '/get_name'
+payload = {"option": "all"}
+x = requests.post(url, json = payload)
+print(x.text)
 
 # url = base_url + '/insert_audio'
 # payload = {"AudioType": "'A'", 
@@ -55,22 +55,22 @@ base_url = "http://127.0.0.1:5000"
 # x = requests.post(url, json = payload)
 # print(x.text)
 
-# url = base_url + '/get_audio'
-# payload = {"AudioId": 1005, "option": "select"}
-# x = requests.post(url, json = payload)
-# print(x.text)
+url = base_url + '/get_audio'
+payload = {"AudioId": 1002, "option": "select"}
+x = requests.post(url, json = payload)
+print(x.text)
 
-# url = base_url + '/get_audio'
-# payload = {"option": "all"}
-# x = requests.post(url, json = payload)
-# print(x.text)
+url = base_url + '/get_audio'
+payload = {"option": "all"}
+x = requests.post(url, json = payload)
+print(x.text)
 
-# url = base_url + '/get_audio_traits'
-# payload = {"TraitId": 3000, "option": "select"}
-# x = requests.post(url, json = payload)
-# print(x.text)
+url = base_url + '/get_audio_traits'
+payload = {"TraitId": 3000, "option": "select"}
+x = requests.post(url, json = payload)
+print(x.text)
 
-# url = base_url + '/get_audio_traits'
-# payload = {"option": "all"}
-# x = requests.post(url, json = payload)
-# print(x.text)
+url = base_url + '/get_audio_traits'
+payload = {"option": "all"}
+x = requests.post(url, json = payload)
+print(x.text)
